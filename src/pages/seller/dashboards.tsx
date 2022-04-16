e2675b2411e23b3dea64fb29c06c62ca70358268
Magno Santos <magno.ufvjm@gmail.com>
2022-04-15 21:36:53 -0300

    feat: change dashboard

--- src/pages/seller/dashboards.tsx
+++ src/pages/seller/dashboards.tsx
@@ -13,7 +13,7 @@ const Dashboards = ({}: Props) => {
           Dashboard
         </Heading>
         <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
-          <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
+          <SimpleGrid flex="1" gap="4" minChildWidth="320px">
             <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
               <Text fontSize="lg" mb="4" color={"white"}>
                 Evolução da quantidade de produtos adicionados por dia
