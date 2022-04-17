4bc0f0169139bc6648d6a0801cd235e8fe19487e
gregorio.luiz <luiz.gregorio@dtidigital.com.br>
2022-04-17 09:26:57 -0300

    Adicinando validação de admin

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,12 +1,32 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 const AdminIndex: React.FC = () => {
+  const userAdmin =
+    typeof window !== "undefined" ? localStorage.getItem("admin") : null;
+  const userAdminAux = userAdmin?.toString();
   const router = useRouter();
   useEffect(() => {
-    router.replace("/admin/sellers");
+    userAdminAux == "admin" ? router.replace("/admin/sellers") : null;
   }, []);
-  return <></>;
+  return (
+    <>
+      <Flex
+        width="100%"
+        alignItems="center"
+        justifyContent="center"
+        marginTop={10}
+      >
+        <Box maxWidth={620}>
+        <Text textAlign="center" fontSize={20} color="red">
+          Você não tem autorização para acessar essa pagina! Por favor, entre em
+          contato com o administrador do sistema.
+        </Text>
+        </Box>
+      </Flex>
+    </>
+  );
 };
 
 export default AdminIndex;
